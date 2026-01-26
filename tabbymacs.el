32b8c40774515c940a02c78874cf928b356478de
Bastillan <kedzierski.jedrzej@gmail.com>
2026-01-26 12:57:38 +0100

    chore: updated version to 1.0.2

--- tabbymacs.el
+++ tabbymacs.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2025 Jędrzej Kędzierski, Warsaw University of Technology, Institute of Computer Science
 
 ;; Author: Jędrzej Kędzierski <kedzierski.jedrzej@gmail.com>
-;; Version: 1.0.1
+;; Version: 1.0.2
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: tools, languages, inline completions, tabby, llm
 ;; URL: https://github.com/Bastillan/tabbymacs
